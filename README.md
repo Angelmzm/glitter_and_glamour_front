4b0085e53a94b12b5aa3b75e28d58827d318643a
angelica <angelicamariazm@gmail.com>
2024-11-21 15:58:53 -0300

    projeto finalizado

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # glitter_and_glamour_front
+Este é um projeto que simula um e-commerce de cosméticos.
+
+## Rode o projeto na sua máquina:
+
+#### Pré-requisitos:
+
+Antes de começar, você vai precisar ter instalado em sua máquina as seguintes ferramentas:
+
+- [Git](https://git-scm.com/downloads)
+
+#### Rodando o projeto:
+
+```bash
+# Clone este repositório
+$ git clone git@github.com:Angelmzm/glitter_and_glamour_front.git
+#Acesse a pasta do projeto no terminal/cmd
+$ cd glitter_and_glamour_front
+```
+
+##### Tecnologias usadas:
+
+- HTML5
+- CSS
+
+##### Bibliotecas e hooks usados:
+
+- ainda não há nenhuma
+
+##### Design:
+
+- [Figma](https://www.figma.com/design/Yz44Q7npMEMouzS2WtKcYl/Glitter%26Glamour?node-id=0-1&t=5BcnE6GILXIGjJWF-1)
+
+##### Créditos:
+
+Desenvolvido por:
+
+- [Angelica-Moreira](https://github.com/Angelmzm)
+
+- [Kellyane-Vieira](https://github.com/Kelly019)
+
+- [Laura-Costa](https://github.com/karnsteinn)
+
+- [Sabrina-Paes](https://github.com/biin4)
+
+- [Thamara-Oliveira](https://github.com/thamara0)
+
+- [Zahira-Silva](https://github.com/ZahiraS2-zz)
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // Define a entrada do aplicativo, onde o Webpack começa a construir o gráfico de dependências
+  entry: './src/index.js', // Substitua pelo caminho do seu arquivo de entrada principal
+
+  // Define onde os arquivos de saída serão colocados após a compilação
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js', // O nome do arquivo compilado
+    publicPath: '/glitter_and_glamour_front/', // Para hospedar o app na subpasta do GitHub Pages
+  },
+
+  // Modo de produção
+  mode: 'production',
+
+  // Configuração dos módulos para manipular diferentes tipos de arquivos
+  module: {
+    rules: [
+      {
+        test: /\.scss$/, // Para arquivos .scss
+        use: [
+          'style-loader', // Injeção de CSS no DOM
+          'css-loader',   // Interpreta CSS
+          'sass-loader',  // Compila o SCSS para CSS
+        ],
+      },
+      {
+        test: /\.js$/, // Para arquivos JS
+        exclude: /node_modules/, // Exclui node_modules
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'], // Transpila o código
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i, // Regra para imagens
+        use: [
+          {
+            loader: 'file-loader', // Usa o file-loader para imagens
+            options: {
+              name: '[name].[hash].[ext]', // Gera um nome de arquivo único baseado no hash
+              outputPath: 'assets/imagens/', // Pasta onde as imagens serão colocadas
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  // Plugins para adicionar funcionalidades adicionais ao Webpack
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'), // Caminho correto para index.html
+    }),
+  ],
+
+  // Definições de ambiente de desenvolvimento
+  devtool: 'source-map', // Para facilitar a depuração, se necessário
+  devServer: {
+    static: path.join(__dirname, 'dist'), // Usando 'static' em vez de 'contentBase' no Webpack 5
+    compress: true,
+    port: 9000,
+    open: true, // Abrir o navegador automaticamente
+    historyApiFallback: true, // Redireciona todas as requisições para index.html
+  },
+};
